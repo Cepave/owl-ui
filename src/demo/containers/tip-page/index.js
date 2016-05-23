919b3a6f79b782e41ea0d9424612d0c12d55086c
RoCky Wu <rwu@cepave.com>
2016-05-23 15:00:09 +0800

    [OWL-600] Tip document

--- src/demo/containers/tip-page/index.js
+++ src/demo/containers/tip-page/index.js
@@ -1,29 +1,28 @@
 import React, {Component} from 'react'
 import Tip from '~com/tip'
 import s from './index.styl'
-import {Validation, ValidEmail} from '~com/validation'
+import {H1, H2, H3, H4, Code, Snippet,
+  Quote, List, Table} from '../../components/markdown'
 
 class TipPage extends React.Component {
   static propTypes = {}
 
   static defaultProps = {}
 
-  submitTest = (e)=> {
-
-  }
-
   render() {
     return (
       <div>
-        <Validation onSubmit={this.submitTest}>
-          <a>test</a>
-          <div>
-            <ValidEmail name="email" regex={/.+@.+/} />
-          </div>
-          <b>bbb</b>
-        </Validation>
+        <H1>Tip</H1>
+        Tip is a <Code>stateless</Code> Component
 
-        <h1>Tip Page</h1>
+        <h2>Usage</h2>
+        <Snippet src={require('./usage.js.raw')} type="js" />
+        
+        <H2>Props</H2>
+        <Snippet src={require('./props.js.raw')} type="js" />
+
+        <H2>Tip on top</H2>
+        <Snippet src={require('./top.html.raw')} type="html" />
         <div>
           <Tip text="tip on top" pos="top">
             <span className={s.dib}>
@@ -32,6 +31,8 @@ class TipPage extends React.Component {
           </Tip>
         </div>
 
+        <H2>Tip on right</H2>
+        <Snippet src={require('./right.html.raw')} type="html" />
         <div>
           <Tip text="tip on right" pos="right">
             <span className={s.dib}>
@@ -40,16 +41,20 @@ class TipPage extends React.Component {
           </Tip>
         </div>
 
+        <H2>Tip on bottom</H2>
+        <Snippet src={require('./bottom.html.raw')} type="html" />
         <div>
-          <Tip text="tip on right" pos="bottom">
+          <Tip text="tip on bottom" pos="bottom">
             <span className={s.dib}>
               Show Tool tip on bottom
             </span>
           </Tip>
         </div>
 
+        <H2>Tip on left</H2>
+        <Snippet src={require('./left.html.raw')} type="html" />
         <div>
-          <Tip text="tip on right" pos="left">
+          <Tip text="tip on left" pos="left">
             <span className={s.dib}>
               Show Tool tip on left
             </span>
