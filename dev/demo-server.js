26f28286f12861e05de793c929ecfad5fc02b91f
RoCky Wu <rwu@cepave.com>
2016-06-08 21:50:06 +0800

    0.0.0.0

--- dev/demo-server.js
+++ dev/demo-server.js
@@ -5,5 +5,5 @@ const cwd = process.cwd()
 app
   .use('/owl-ui', express.static(`${cwd}/demo`))
   .listen(3030, ()=> {
-    console.log(`Demo server is running on: 0.0.0.:3030/owl-ui`)
+    console.log(`Demo server is running on: 0.0.0.0:3030/owl-ui`)
   })
