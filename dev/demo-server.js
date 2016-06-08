811ed854dfb116c1a50c7a841af5170977b2eebd
RoCky Wu <rwu@cepave.com>
2016-06-08 17:56:58 +0800

    add `demo.server` script

---
+++ dev/demo-server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+const app = express()
+const cwd = process.cwd()
+
+app
+  .use('/owl-ui', express.static(`${cwd}/demo`))
+  .listen(3030, ()=> {
+    console.log(`Demo server is running on: 0.0.0.:3030/owl-ui`)
+  })
