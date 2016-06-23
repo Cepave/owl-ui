ba571174e26ee88b031bbd0ebd6156fb197b15f3
RoCky Wu <rwu@cepave.com>
2016-06-23 21:10:12 +0800

    log url

--- src/demo/server/index.js
+++ src/demo/server/index.js
@@ -11,5 +11,5 @@ server
   .use('/static', express.static(`${cwd}/static`))
   .use(require('./html-mid'))
   .listen(3000, ()=> {
-    console.log(`🌎 Server is listening ${c.cyan(3000)}`)
+    console.log(`🌎 Server is running: 0.0.0.0:${c.cyan(3000)}/owl-ui/`)
   })
